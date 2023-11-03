a1db115a9930422c9fc4c098f66e50bb000d9f17
Juan Pablo Dulcey Sanabria <121118662+syz7gy@users.noreply.github.com>
2023-11-03 16:21:45 -0500

    Initial commit

---
+++ src/main/java/co/edu/unbosque/bestrestaurant/BestrestaurantApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.bestrestaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestrestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestrestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/bestrestaurant/BestrestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.bestrestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestrestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
